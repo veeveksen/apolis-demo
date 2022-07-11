66fe4bdf6d1fb22c6f938de34faea7f2daaf8430
vivek <vivek.sen@ticketmaster.com>
2022-07-11 14:44:52 +0530

    initial commit

---
+++ src/main/java/com/apolis/ApolisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.apolis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApolisDemoApplication.class, args);
+	}
+
+}
