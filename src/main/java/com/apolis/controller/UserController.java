044f6e5ed37cd994d36993fb538b630173d97663
vivek <vivek.sen@ticketmaster.com>
2022-07-11 15:05:11 +0530

    initial commit

---
+++ src/main/java/com/apolis/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.apolis.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @RequestMapping("/user")
+    public String getUser(){
+        return "Vivek";
+    }
+}
